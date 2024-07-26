7e50456c6e06973360da54497ab4302511e24bc6
ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-07-26 22:36:58 +0530

    new structure and env updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chest_Disease',
+    version= '0.0.1',
+    author= 'Ritik Padhi',
+    author_email= 'Ritikpadhi1507@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
